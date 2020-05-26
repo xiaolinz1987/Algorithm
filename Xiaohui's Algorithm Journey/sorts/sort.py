5174059aeab0b6239dd9d079534e69ca274974a5
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-05-26 16:47:10 +0800

    Implement bubble sort, version 3 - with sort_border and is_sorted flag

--- Xiaohui's Algorithm Journey/sorts/sort.py
+++ Xiaohui's Algorithm Journey/sorts/sort.py
@@ -25,6 +25,24 @@ class MySort:
             if is_sorted:
                 break
 
+    def bubble_sort_v3(self):
+        last_exchange_index = 0
+        sort_border = len(self.array) - 1
+        
+        for i in range(len(self.array)-1):
+            is_sorted = True
+            for j in range(sort_border):
+                if self.array[j] > self.array[j+1]:
+                    temp = self.array[j]
+                    self.array[j] = self.array[j+1]
+                    self.array[j+1] = temp
+                    is_sorted = False
+                    last_exchange_index = j
+            
+            sort_border = last_exchange_index
+            if is_sorted:
+                break
+
     def output(self):
         print(self.array)
 
@@ -36,5 +54,9 @@ sort.output()
 sort.reset_input(input)
 sort.bubble_sort_v2()
 print("bubble_sort_v2:")
+sort.output()
+sort.reset_input(input)
+sort.bubble_sort_v3()
+print("bubble_sort_v3:")
 sort.output()
 
