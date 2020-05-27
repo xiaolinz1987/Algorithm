db9fcbdfc4445106ed915dd728869b75c63a838a
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-05-27 16:03:51 +0800

    Implement quick sort, version 2 with single loop with mark

--- Xiaohui's Algorithm Journey/sorts/quicksort.py
+++ Xiaohui's Algorithm Journey/sorts/quicksort.py
@@ -26,22 +26,50 @@ class MyQuickSort:
         self.array[left] = pivot
         return left
     
-    def __quick_sort_core(self, start_index, end_index):
+    def __quick_sort_core_v1(self, start_index, end_index):
         if start_index >= end_index:
             return
         pivot_index = self.__partition_v1(start_index, end_index)
-        self.__quick_sort_core(start_index, pivot_index - 1)
-        self.__quick_sort_core(pivot_index + 1, end_index)
+        self.__quick_sort_core_v1(start_index, pivot_index - 1)
+        self.__quick_sort_core_v1(pivot_index + 1, end_index)
 
-    def quick_sort(self):
-        self.__quick_sort_core(0, len(self.array)-1)
+    def __partition_v2(self, start_index, end_index):
+        pivot = self.array[start_index]
+        mark = start_index
+
+        for i in range(start_index+1, end_index+1):
+            if self.array[i] < pivot:
+                mark += 1
+                temp = self.array[mark]
+                self.array[mark] = self.array[i]
+                self.array[i] = temp
+
+        self.array[start_index] = self.array[mark]
+        self.array[mark] = pivot
+        return mark
+
+    def __quick_sort_core_v2(self, start_index, end_index):
+        if start_index >= end_index:
+            return
+        pivot_index = self.__partition_v2(start_index, end_index)
+        self.__quick_sort_core_v2(start_index, pivot_index - 1)
+        self.__quick_sort_core_v2(pivot_index + 1, end_index)
+
+    def quick_sort_v1(self):
+        self.__quick_sort_core_v1(0, len(self.array)-1)
+
+    def quick_sort_v2(self):
+        self.__quick_sort_core_v2(0, len(self.array)-1)
 
     def output(self):
         print(self.array)
 
 input = list([3, 4, 14, 1, 5, 6, 7, 8, 1, -1, 0, 9, 11])
 sort = MyQuickSort(input)
-sort.quick_sort()
-print("quick_sort v1:")
+sort.quick_sort_v1()
+print("quick_sort_v1:")
 sort.output()
-
+sort.reset_input(input)
+sort.quick_sort_v2()
+print("quick_sort_v2:")
+sort.output()
