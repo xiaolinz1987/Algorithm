5db6dbd33d1d4a9401eb245d2578404c4c309181
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-05-27 16:39:09 +0800

    Implement quick sort, version 3 with non-recursive

--- Xiaohui's Algorithm Journey/sorts/quicksort.py
+++ Xiaohui's Algorithm Journey/sorts/quicksort.py
@@ -55,15 +55,37 @@ class MyQuickSort:
         self.__quick_sort_core_v2(start_index, pivot_index - 1)
         self.__quick_sort_core_v2(pivot_index + 1, end_index)
 
+    def __quick_sort_core_v3(self, start_index, end_index):
+        quick_sort_stack = []
+        root_param = {"startIndex": start_index, "endIndex": end_index}
+        quick_sort_stack.append(root_param)
+
+        while len(quick_sort_stack) > 0:
+            param = quick_sort_stack.pop()
+            param_start_index = param.get("startIndex")
+            param_end_index = param.get("endIndex")
+            pivot_index = self.__partition_v2(param_start_index, param_end_index)
+            
+            if param_start_index < pivot_index - 1:
+                left_param = {"startIndex": param_start_index, "endIndex": pivot_index - 1}
+                quick_sort_stack.append(left_param)
+            if param_end_index > pivot_index + 1:
+                right_param = {"startIndex": pivot_index + 1, "endIndex": param_end_index}
+                quick_sort_stack.append(right_param)
+
     def quick_sort_v1(self):
         self.__quick_sort_core_v1(0, len(self.array)-1)
 
     def quick_sort_v2(self):
         self.__quick_sort_core_v2(0, len(self.array)-1)
 
+    def quick_sort_v3(self):
+        self.__quick_sort_core_v3(0, len(self.array)-1)
+
     def output(self):
         print(self.array)
 
+"""
 input = list([3, 4, 14, 1, 5, 6, 7, 8, 1, -1, 0, 9, 11])
 sort = MyQuickSort(input)
 sort.quick_sort_v1()
@@ -73,3 +95,8 @@ sort.reset_input(input)
 sort.quick_sort_v2()
 print("quick_sort_v2:")
 sort.output()
+sort.reset_input(input)
+sort.quick_sort_v3()
+print("quick_sort_v3:")
+sort.output()
+"""
