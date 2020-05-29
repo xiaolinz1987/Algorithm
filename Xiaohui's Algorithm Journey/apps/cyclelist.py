8a377f4092b5ef280d9757e752b222f85ebbbfb5
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-05-29 14:49:58 +0800

    Check if a list has cycle, O(n) for time complexity and O(1) for space complexity

---
+++ Xiaohui's Algorithm Journey/apps/cyclelist.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append('../')
+from utils.linkedlist import Node
+
+def is_cycle_list(head):
+    p1 = head
+    p2 = head
+
+    while p2 is not None and p2.next is not None:
+        p1 = p1.next
+        p2 = p2.next.next
+
+        if p1 == p2:
+            return True
+
+    return False
+
+node1 = Node(5)
+node2 = Node(3)
+node3 = Node(7)
+node4 = Node(2)
+node5 = Node(6)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+print(is_cycle_list(node1))
+node5.next = node2
+print(is_cycle_list(node1))
+
