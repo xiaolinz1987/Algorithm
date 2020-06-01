4df72a8c1401d84b588ece477046f85686dcd71d
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-06-01 14:51:15 +0800

    Implement greatest common divisor, 4 versions

---
+++ Xiaohui's Algorithm Journey/apps/greatestcommondivisor.py
@@ -0,0 +1,88 @@
+class MyGreatestCommonDivisor:
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        self.greatest_common_divisor = 1
+
+    def set(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def calculate_v1(self):
+        big = max(self.num1, self.num2)
+        small = min(self.num1, self.num2)
+        if big % small == 0:
+            self.greatest_common_divisor = small
+            return
+
+        for i in range(small//2, 1, -1):
+            if small % i == 0 and big % i == 0:
+                self.greatest_common_divisor = i
+                return
+
+    def __calculate_v2_core(self, num1, num2):
+        big = max(num1, num2)
+        small = min(num1, num2)
+        if big % small == 0:
+            return small
+
+        return self.__calculate_v2_core(big % small, small)
+
+    def calculate_v2(self):
+        self.greatest_common_divisor = self.__calculate_v2_core(self.num1, self.num2)
+
+    def __calculate_v3_core(self, num1, num2):
+        if num1 == num2:
+            return num1
+
+        big = max(num1, num2)
+        small = min(num1, num2)
+        return self.__calculate_v3_core(big - small, small)
+
+    def calculate_v3(self):
+        self.greatest_common_divisor = self.__calculate_v3_core(self.num1, self.num2)
+
+    def __calculate_v4_core(self, num1, num2):
+        if num1 == num2:
+            return num1
+
+        if (num1 & 1) == 0 and (num2 & 1) == 0:
+            return self.__calculate_v4_core(num1 >> 1, num2 >> 1) << 1
+        elif (num1 & 1) == 0 and (num2 & 1) != 0:
+            return self.__calculate_v4_core(num1 >> 1, num2)
+        elif (num1 & 1) != 0 and (num2 & 1) == 0:
+            return self.__calculate_v4_core(num1, num2 >> 1)
+        else:
+            big = max(num1, num2)
+            small = min(num1, num2)
+            return self.__calculate_v4_core(big - small, small)
+
+    def calculate_v4(self):
+        self.greatest_common_divisor = self.__calculate_v4_core(self.num1, self.num2)
+
+    def output(self):
+        print(self.greatest_common_divisor)
+
+greatestcommondivisor = MyGreatestCommonDivisor(25, 5)
+greatestcommondivisor.calculate_v1()
+print("greatest_common_divisor v1:")
+greatestcommondivisor.output()
+greatestcommondivisor.set(100, 75)
+greatestcommondivisor.calculate_v2()
+print("greatest_common_divisor v2:")
+greatestcommondivisor.output()
+greatestcommondivisor.set(99, 55)
+greatestcommondivisor.calculate_v3()
+print("greatest_common_divisor v3:")
+greatestcommondivisor.output()
+greatestcommondivisor.set(10000, 1)
+greatestcommondivisor.calculate_v4()
+print("greatest_common_divisor v4:")
+greatestcommondivisor.output()
+greatestcommondivisor.set(10000, 10001)
+greatestcommondivisor.calculate_v4()
+print("greatest_common_divisor v4:")
+greatestcommondivisor.output()
+
+
