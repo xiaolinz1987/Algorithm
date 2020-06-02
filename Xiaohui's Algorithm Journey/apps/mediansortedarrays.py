46280ea8e7e9b59175f1514235a60579f3353dbc
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-06-02 15:26:35 +0800

    Implement median of 2 sorted arrays

---
+++ Xiaohui's Algorithm Journey/apps/mediansortedarrays.py
@@ -0,0 +1,54 @@
+class MyMedianSortedArray:
+
+    def __init__(self, input_array_1, input_array_2):
+        self.array1 = input_array_1
+        self.array2 = input_array_2
+        self.median = 0
+
+    def calculate(self):
+        m, n = len(self.array1), len(self.array2)
+        if m > n:
+            self.array1, self.array2, m, n = self.array2, self.array1, n, m
+        if n == 0:
+            raise ValueError
+
+        start, end, half_len = 0, m, (m + n + 1)//2
+        while start <= end:
+            i = (start + end)//2
+            j = half_len - i
+
+            if i < m and self.array2[j-1] > self.array1[i]:
+                start = i + 1
+            elif i > 0 and self.array1[i-1] > self.array2[j]:
+                end = i - 1
+            else:
+                if i == 0:
+                    max_of_left = self.array2[j-1]
+                elif j == 0:
+                    max_of_left = self.array1[i-1]
+                else:
+                    max_of_left = max(self.array1[i-1], self.array2[j-1])
+
+                if (m + n)%2 == 1:
+                    self.median = max_of_left
+                    return
+
+                if i == m:
+                    min_of_right = self.array2[j]
+                elif j == n:
+                    min_of_right = self.array1[i]
+                else:
+                    min_of_right = min(self.array1[i], self.array2[j])
+
+                self.median = (max_of_left + min_of_right) / 2.0
+                return
+
+    def output(self):
+        print(self.median)
+
+array1 = list([3, 5, 6, 7, 8, 12, 20])
+array2 = list([1, 10, 17, 18])
+median = MyMedianSortedArray(array1, array2)
+median.calculate()
+median.output()
+
