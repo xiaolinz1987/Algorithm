1743faf9add1c2a4cf9e875efa6d446ab8c164a7
xiaolinz1987 <xlzhu1987@aliyun.com>
2020-06-04 16:05:46 +0800

    Implement LRU cache with double linkedlist

---
+++ Xiaohui's Algorithm Journey/projects/lcucache.py
@@ -0,0 +1,86 @@
+class Node:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.pre = None
+        self.next = None
+
+class MyLRUCache:
+
+    def __init__(self, limit):
+        self.limit = limit
+        self.hash = {}
+        self.head = None
+        self.end = None
+
+    def __add_node(self, node):
+        if self.end is not None:
+            self.end.next = node
+            node.pre = self.end
+            node.next = None
+        self.end = node
+        if self.head is None:
+            self.head = node
+
+    def __remove_node(self, node):
+        if node == self.head and node == self.end:
+            self.head = None
+            self.end = None
+        elif node == self.end:
+            self.end = self.end.pre
+            self.end.next = None
+        elif node == self.head:
+            self.head = self.head.next
+            self.head.pre = None
+        else:
+            node.pre.next = node.pre
+            node.next.pre = node.pre
+        return node.key
+
+    def __refresh_node(self, node):
+        if node == self.end:
+            return
+        self.__remove_node(node)
+        self.__add_node(node)
+
+    def get(self, key):
+        node = self.hash.get(key)
+        if node is None:
+            return None
+        
+        self.__refresh_node(node)
+        return node.value
+
+    def put(self, key, value):
+        node = self.hash.get(key)
+        if node is None:
+            if len(self.hash) >= self.limit:
+                old_key = self.__remove_node(self.head)
+                self.hash.pop(old_key)
+            
+            node = Node(key, value)
+            self.__add_node(node)
+            self.hash[key] = node
+        else:
+            node.value = value
+            self.__refresh_node(node)
+
+    def remove(self, key):
+        node = self.hash.get(key)
+        self.__remove_node(node)
+        self.hash.remove(key)
+
+lru = MyLRUCache(5)
+lru.put("001", "User1")
+print(lru.get("001"))
+lru.put("002", "User2")
+lru.put("003", "User3")
+lru.put("004", "User4")
+lru.put("005", "User5")
+print(lru.get("002"))
+lru.put("004", "User4 update")
+lru.put("006", "User6")
+print(lru.get("001"))
+print(lru.get("006"))
+
